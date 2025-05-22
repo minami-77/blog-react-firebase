1f952547223d0ba2c9633134a51a95e701884137
Minami Takayama <m.inam.ina03037@gmail.com>
2025-05-22 18:18:33 +0900

    can login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from "../firebase";
+import { useNavigate } from 'react-router';
 
-const Login = () => {
+const Login = ({setIsAuth}) => {
+  const navigate = useNavigate();
+  const loginWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result)=>{
+      console.log(result);
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+
+  };
+
   return (
-    <div>Login</div>
+    <div>
+      <p>ログインしてはじめる</p>
+      <button onClick={loginWithGoogle}>Googleでログイン</button>
+    </div>
   )
 }
 
