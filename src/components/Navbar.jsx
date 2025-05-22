11d63e9b8f3d8d1ceed0a640095cada02fff29ad
Minami Takayama <m.inam.ina03037@gmail.com>
2025-05-22 17:36:10 +0900

    add font awsome

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { Link } from "react-router";
+import "./Navbar.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faPenToSquare,faRightToBracket } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">ホーム</Link>
-      <Link to="/creatpost">記事投稿</Link>
-      <Link to="/login">ログイン</Link>
+      <Link to="/">
+        <FontAwesomeIcon icon={faHouse} />
+        ホーム
+      </Link>
+      <Link to="/creatpost">
+        <FontAwesomeIcon icon={faPenToSquare} />
+        記事投稿
+      </Link>
+      <Link to="/login">
+        <FontAwesomeIcon icon={faRightToBracket} />
+        ログイン
+      </Link>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
