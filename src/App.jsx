752283573dbb0fb3bc70dc51b500ec2043b832d0
Minami Takayama <m.inam.ina03037@gmail.com>
2025-05-22 16:21:32 +0900

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css'
+// import ReactDOM from "react-dom/client";
+import {BrowserRouter as Router, Routes, Route, Link } from "react-router";
+import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import Login from './components/Login';
+import Logout from './components/Logout';
 
 function App() {
 
 
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/createpost" element={<CreatePost/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/logout" element={<Logout/>}></Route>
+      </Routes>
 
-    </>
-  )
+    </Router>
+    )
 }
 
 export default App
