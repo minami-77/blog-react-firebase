c3d1d81bad68712edd12209ab805f894fea5ab8b
Minami Takayama <m.inam.ina03037@gmail.com>
2025-05-23 22:10:02 +0900

    can change login logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse, faPenToSquare,faRightToBracket } from '@fortawesome/free-solid-svg-icons'
 
-const Navbar = () => {
+const Navbar = ({isAuth}) => {
   return (
     <nav>
       <Link to="/">
@@ -15,10 +15,19 @@ const Navbar = () => {
         <FontAwesomeIcon icon={faPenToSquare} />
         記事投稿
       </Link>
-      <Link to="/login">
-        <FontAwesomeIcon icon={faRightToBracket} />
-        ログイン
-      </Link>
+      {!isAuth ? (
+        <Link to="/login">
+          <FontAwesomeIcon icon={faRightToBracket} />
+          ログイン
+        </Link>
+        ) : (
+          <Link to="/logout">
+            <FontAwesomeIcon icon={faRightToBracket} />
+            ログアウト
+          </Link>
+        )
+      }
+
     </nav>
   )
 }
