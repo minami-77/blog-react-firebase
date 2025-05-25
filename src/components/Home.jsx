71d6bf98d9b85639c667aaebb14b7cdc0b903020
Minami Takayama <m.inam.ina03037@gmail.com>
2025-05-25 15:05:02 +0900

    can display exracted input to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Home.css";
 import { db } from "../firebase";
-import { collection, doc, getDocs } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 
 
 const Home = () => {
+const [postList, setPostList] = useState([]);
+
   // 一度だけ発火のため、第二引数は空にする
   useEffect(()=>{
     const getPosts = async () => {
       const data = await getDocs(collection (db,"posts"))
-
-      console.log(data);
-      console.log(data.docs);
-      console.log(data.docs.map((doc)=>({ doc })));
+      // console.log(data);
+      // console.log(data.docs);
+      // console.log(data.docs.map((doc)=>({ doc })));
       // データ関数.data(),スプレッド構文
-            console.log(data.docs.map((doc)=>({ ...doc.data(),id: doc.id })));
+      console.log(data.docs.map((doc)=>({ ...doc.data(),id: doc.id })));
+
+      setPostList(data.docs.map((doc)=>({ ...doc.data(),id: doc.id })));
     };
     getPosts();
   }, [])
+
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
+      {postList.map((post)=>{
+        return (
+        <div className="postContents" key={post.id}>
+          <div className="postHeader">
+            <h1>{post.title}</h1>
+          </div>
+          <div className="postTextContainer">
+            {post.postsText}
+          </div>
+          <div className="nameAndDeleteButton">
+            <h3>@{post.author.username}</h3>
+            <button>Delete</button>
+          </div>
         </div>
-      <div className="postTextContainer">
-        今はReactを勉強中です。今はReactを勉強中です。今はReactを勉強中です。今はReactを勉強中です。
-      </div>
-      <div className="nameAndDeleteButton">
-        <h3>@Minami</h3>
-        <button>Delete</button>
-      </div>
+        )
 
-      </div>
+      })}
+
 
     </div>
   )
