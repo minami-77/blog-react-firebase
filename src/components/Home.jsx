f185d02d0c7efb52bb8ba4514b02457785cb41b9
Minami Takayama <m.inam.ina03037@gmail.com>
2025-05-25 11:50:32 +0900

    add homepage contents

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+      <div className="postTextContainer">
+        今はReactを勉強中です。今はReactを勉強中です。今はReactを勉強中です。今はReactを勉強中です。
+      </div>
+      <div className="nameAndDeleteButton">
+        <h3>@Minami</h3>
+        <button>Delete</button>
+      </div>
+
+      </div>
+
+    </div>
   )
 }
 
