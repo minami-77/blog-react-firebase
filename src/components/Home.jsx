7319354dcbfe925fd48655fb38614a52baeea973
Minami Takayama <m.inam.ina03037@gmail.com>
2025-05-25 14:55:23 +0900

    extract input

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import "./Home.css";
+import { db } from "../firebase";
+import { collection, doc, getDocs } from 'firebase/firestore';
+
 
 const Home = () => {
+  // 一度だけ発火のため、第二引数は空にする
+  useEffect(()=>{
+    const getPosts = async () => {
+      const data = await getDocs(collection (db,"posts"))
+
+      console.log(data);
+      console.log(data.docs);
+      console.log(data.docs.map((doc)=>({ doc })));
+      // データ関数.data(),スプレッド構文
+            console.log(data.docs.map((doc)=>({ ...doc.data(),id: doc.id })));
+    };
+    getPosts();
+  }, [])
   return (
     <div className="homePage">
       <div className="postContents">
